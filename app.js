641eebe311fa84b9e30dc8a4b483343da888e7f4
Mike <mclancy96@gmail.com>
2021-05-12 14:40:22 -0400

    Inital setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req,res) => {
+	res.render("./index", {});
+});
+
+app.listen(process.env.PORT, function(){
+	console.log("SEMS Member Site has started...");
+});
