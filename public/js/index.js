b05e0633c357413b43a2e88e60ea09e7faf90818
Mike <mclancy96@gmail.com>
2021-05-14 13:12:59 -0400

    Update

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,6 @@
-// let getDocumentHeight = () => {
-//     return Math.max(
-//         Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
-//         Math.max(document.body.offsetHeight, document.documentElement.offsetHeight),
-//         Math.max(document.body.clientHeight, document.documentElement.clientHeight)
-//     );
-// }
-
-// $(window).scroll(() => {
-//     var docHeight = getDocumentHeight();
-//     if($(window).scrollTop() + window.innerHeight == docHeight){
-//                 // enter your code here
-//     }
-// });
-
-
 $(window).on("load",() => {
     $(window).scroll(() => {
-        // var docHeight = getDocumentHeight();
         let windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        // console.log("docHeight: ", docHeight);
         console.log("windowBottom: ", windowBottom)
         $(".scroll-fade").each(function() {
             let objectTop = $(this).offset().top;
