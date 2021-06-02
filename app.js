c3b5029ae623ea29ac0cf073b70d39f7133084eb
Mike <mclancy96@gmail.com>
2021-06-02 12:36:27 -0400

    Add weather dashboard

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express"),
-    app = express();
+    app = express(),
+    axios = require('axios');
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req,res) => {
+    axios.get('https://sems.mikeclancy.me');
+    axios.get('https://tgt.mikeclancy.me');
+    axios.get('https://bgg.mikeclancy.me');
+    axios.get('https://wdash.mikeclancy.me')
 	res.render("./index", {});
 });
 
