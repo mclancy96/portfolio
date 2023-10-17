919c8069fc6df1e1f1160e941c1d9cbf44391430
Mike <clancymi@oregonstate.edu>
2023-10-17 18:52:37 -0400

    Comments out the axios requests for renovation

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const express = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req,res) => {
-    axios.get('https://sems.mikeclancy.me').catch(err=>console.log(err));
-    axios.get('https://tgt.mikeclancy.me').catch(err=>console.log(err));
-    axios.get('https://bgg.mikeclancy.me').catch(err=>console.log(err));
-    axios.get('https://wdash.mikeclancy.me').catch(err=>console.log(err))
-	res.render("./index", {});
+app.get("/", (req, res) => {
+    // axios.get('https://sems.mikeclancy.me').catch(err=>console.log(err));
+    // axios.get('https://tgt.mikeclancy.me').catch(err=>console.log(err));
+    // axios.get('https://bgg.mikeclancy.me').catch(err=>console.log(err));
+    // axios.get('https://wdash.mikeclancy.me').catch(err=>console.log(err))
+    res.render("./index", {});
 });
 
-app.listen(process.env.PORT, function(){
-	console.log("Portfolio has started...");
+app.listen(process.env.PORT, function () {
+    console.log("Portfolio has started...");
     console.log("Let me know what you think!")
 });
