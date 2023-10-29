0909a7e1c569618403d24f663d5db82ddab803e3
Mike Clancy <mclancy96@gmail.com>
2023-10-29 09:43:00 -0400

    Adds icons to about me

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express = require("express"),
-    app = express(),
-    axios = require('axios');
+const express = require("express");
+const dotenv = require('dotenv');
+const app = express();
+const axios = require('axios');
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+dotenv.config();
+const iconLink = process.env.ICONS
 
 app.get("/", (req, res) => {
     // axios.get('https://sems.mikeclancy.me').catch(err=>console.log(err));
     // axios.get('https://tgt.mikeclancy.me').catch(err=>console.log(err));
     // axios.get('https://bgg.mikeclancy.me').catch(err=>console.log(err));
     // axios.get('https://wdash.mikeclancy.me').catch(err=>console.log(err))
-    res.render("./index", {});
+    res.render("index", { iconLink: iconLink });
 });
 
 app.listen(process.env.PORT, function () {
